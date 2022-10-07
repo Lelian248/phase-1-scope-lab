0651c013dac1d10688445826c6e52caf5824d33b
lilian manaa <lelian.ibraheem@gmail.com>
2022-10-07 18:27:15 +0300

    3 test couldn't fix

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var customerName ='bob';
+
+function upperCaseCustomerName(){
+    var name =customerName.toUpperCase();
+    return name;
+}
+
+/*function upperCaseCustomerName(){
+    return customerName= customerName.toUpperCase();;
+}*/
+console.log(customerName)
+console.log(upperCaseCustomerName())
+console.log(customerName)
+
+
+function setBestCustomer(){
+    var bestCustomer ='not bob'
+    return bestCustomer;
+}
+
+console.log(setBestCustomer())
+
+function overwriteBestCustomer (){
+    var bestCustomer ='maybe bob'
+    return bestCustomer;
+}
+
+console.log(overwriteBestCustomer())
+
+const leastFavoriteCustomer= 'tom'
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer='Assignment to constant variable.'
+}
